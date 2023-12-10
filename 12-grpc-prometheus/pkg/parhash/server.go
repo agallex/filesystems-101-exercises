2956db095109a9d157d5eaa074329ac99abaf7f2
agallex <asasha2017@yandex.ru>
2023-12-11 00:34:33 +0300

    12

--- 12-grpc-prometheus/pkg/parhash/server.go
+++ 12-grpc-prometheus/pkg/parhash/server.go
@@ -2,10 +2,16 @@ package parhash
 
 import (
 	"context"
+	"net"
+	"sync"
 
 	"github.com/pkg/errors"
-	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/sync/semaphore"
+	"google.golang.org/grpc"
+
+	"fs101ex/pkg/gen/hashsvc"
+	"fs101ex/pkg/gen/parhashsvc"
+	"fs101ex/pkg/workgroup"
 )
 
 type Config struct {
@@ -16,67 +22,131 @@ type Config struct {
 	Prom prometheus.Registerer
 }
 
-// Implement a server that responds to ParallelHash()
-// as declared in /proto/parhash.proto.
-//
-// The implementation of ParallelHash() must not hash the content
-// of buffers on its own. Instead, it must send buffers to backends
-// to compute hashes. Buffers must be fanned out to backends in the
-// round-robin fashion.
-//
-// For example, suppose that 2 backends are configured and ParallelHash()
-// is called to compute hashes of 5 buffers. In this case it may assign
-// buffers to backends in this way:
-//
-//	backend 0: buffers 0, 2, and 4,
-//	backend 1: buffers 1 and 3.
-//
-// Requests to hash individual buffers must be issued concurrently.
-// Goroutines that issue them must run within /pkg/workgroup/Wg. The
-// concurrency within workgroups must be limited by Server.sem.
-//
-// WARNING: requests to ParallelHash() may be concurrent, too.
-// Make sure that the round-robin fanout works in that case too,
-// and evenly distributes the load across backends.
-//
-// The server must report the following performance counters to Prometheus:
-//
-//  1. nr_nr_requests: a counter that is incremented every time a call
-//     is made to ParallelHash(),
-//
-//  2. subquery_durations: a histogram that tracks durations of calls
-//     to backends.
-//     It must have a label `backend`.
-//     Each subquery_durations{backed=backend_addr} must be a histogram
-//     with 24 exponentially growing buckets ranging from 0.1ms to 10s.
-//
-// Both performance counters must be placed to Prometheus namespace "parhash".
 type Server struct {
 	conf Config
 
 	sem *semaphore.Weighted
+
+	l          net.Listener
+	wg         sync.WaitGroup
+	stop       context.CancelFunc
+	mutex      sync.Mutex
+	currClient int
+
+	nr_requests        prometheus.Counter
+    subquery_durations *prometheus.HistogramVec
 }
 
 func New(conf Config) *Server {
 	return &Server{
 		conf: conf,
 		sem:  semaphore.NewWeighted(int64(conf.Concurrency)),
+		nr_requests: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: "parhash",
+			Name:      "nr_requests",
+		}),
+		subquery_durations: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Namespace: "parhash",
+			Name:      "subquery_durations",
+			Buckets:   prometheus.ExponentialBuckets(0.1, 10*1000, 24),
+		}, []string{"backend"}),
 	}
 }
 
 func (s *Server) Start(ctx context.Context) (err error) {
 	defer func() { err = errors.Wrap(err, "Start()") }()
 
-	/* implement me */
+    ctx, s.stop = context.WithCancel(ctx)
+    s.conf.Prom.MustRegister(s.nr_requests)
+    s.conf.Prom.MustRegister(s.subquery_durations)
+
+	s.l, err = net.Listen("tcp", s.conf.ListenAddr)
+	if err != nil {
+		return err
+	}
+
+	srv := grpc.NewServer()
+	parhashsvc.RegisterParallelHashSvcServer(srv, s)
+
+	s.wg.Add(2)
+
+	go func() {
+		defer s.wg.Done()
+
+		srv.Serve(s.l)
+	}()
+
+	go func() {
+		defer s.wg.Done()
+
+		<-ctx.Done()
+		s.l.Close()
+	}()
 
 	return nil
 }
 
 func (s *Server) ListenAddr() string {
-	/* implement me */
-	return ""
+	return s.l.Addr().String()
 }
 
 func (s *Server) Stop() {
-	/* implement me */
+	s.stop()
+	s.wg.Wait()
+}
+
+func (s *Server) ParallelHash(ctx context.Context, req *parhashsvc.ParHashReq) (resp *parhashsvc.ParHashResp, err error) {
+	defer func() { err = errors.Wrapf(err, "ParallelHash()") }()
+
+    s.nr_requests.Inc()
+
+    workers := workgroup.New(workgroup.Config{Sem: s.sem})
+    hashes := make([][]byte, len(req.Data))
+	backendsCnt := len(s.conf.BackendAddrs)
+	cls := make([]hashsvc.HashSvcClient, backendsCnt)
+	connections := make([]*grpc.ClientConn, backendsCnt)
+
+	for k, address := range s.conf.BackendAddrs {
+		connections[k], err = grpc.Dial(address, grpc.WithInsecure())
+		if err != nil {
+			return nil, err
+		}
+		defer connections[k].Close()
+		cls[k] = hashsvc.NewHashSvcClient(connections[k])
+	}
+
+	for k, buffer := range req.Data {
+		currReq := k
+		currBuffer := buffer
+
+		workers.Go(ctx, func(ctx context.Context) error {
+			s.mutex.Lock()
+			currClient := s.currClient
+			s.currClient = (s.currClient + 1) % backendsCnt
+			s.mutex.Unlock()
+
+			start_time := time.Now()
+			bR, err := cls[currClient].Hash(ctx, &hashsvc.HashReq{Data: currBuffer})
+			passed_time := time.Since(start_time)
+
+			if err != nil {
+				return err
+			}
+
+			s.mutex.Lock()
+			hashes[currReq] = bR.Hash
+			s.mutex.Unlock()
+
+			s.subquery_durations.With(prometheus.Labels{"backend": s.conf.BackendAddrs[currClient]}).Observe(float64(passed_time.Milliseconds()))
+
+			return nil
+		})
+	}
+
+	err = workers.Wait()
+	if err != nil {
+		return nil, err
+	}
+
+	return &parhashsvc.ParHashResp{Hashes: hashes}, nil
 }
